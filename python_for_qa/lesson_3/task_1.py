24f97d879db5d40d82394765c83ec71c74d94896
Stepan_Pelykh <stepan_pelykh@epam.com>
2016-06-01 14:19:19 +0300

    add new script

--- python_for_qa/lesson_3/task_1.py
+++ python_for_qa/lesson_3/task_1.py
@@ -1,1 +1,13 @@
 __author__ = 'Stepan_Pelykh'
+
+
+# list comprehension
+
+num = [i for i in range(1, 10000) if i % 2 == 0 and i % 3 == 0]
+print(num)
+
+# filter function
+
+num1 = filter(lambda x: x % 2 == 0 and x % 3 == 0, range(1, 10000))
+print(num1)
+
